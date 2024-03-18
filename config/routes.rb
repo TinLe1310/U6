846728c6053d21c051267da9ed3ea92ebd718e88
Tin Le <tintrungle1310@gmail.com>
2024-03-18 17:26:57 -0500

    Create Controller and Views for Customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root to: "customers#index"
+  resources :customers, only: [:index] do
+    collection do
+      get :alphabetized
+      get :missing_email
+    end
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
